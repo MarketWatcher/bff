9651349f369e6037e3ca52bb776a02740989d2ba
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2016-05-20 12:08:12 +0300

    #1 Story @emrah-b Main View adding react components to get ready for react transformation

---
+++ src/components/main-layout.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react';
+
+export class MainComponent extends Component {
+
+    render() {
+        return(
+            <div> Hello, MarketWatcher!</div>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MainComponent from './components/MainComponent';
+
+render(){
+	<MainComponent />,
+document.getElementById('content')
+
+}
+
+
