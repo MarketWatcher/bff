404368dc976713f7c8b1543513deb3a9a1f41f74
seyma <seyma@seymas-MacBook-Pro.local>
2016-06-06 12:52:55 +0300

    #8 @sinan-si changes webpack _base webpack config

--- config/webpack/_base.js
+++ config/webpack/_base.js
@@ -8,7 +8,7 @@ module.exports = {
 		app: ["./src/index.js"]
 	},
 	output: {
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "../../dist"),
 		publicPath: "/",
 		filename: "[name].[hash].js"
 	},
