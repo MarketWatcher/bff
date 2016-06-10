23d13d6f211acb224b9c0b630c05d3057d4b41d8
Cengiz Can <cengizc@gmail.com>
2016-06-10 22:59:53 +0300

    #4 remove unnecessary import

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import { Component } from 'react';
 import * as actionCreators  from "../actions";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
