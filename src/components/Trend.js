5f8f837a4480772f59691c12a519ec12f89b2fcb
Emrah B <emrahb@gmail.com>
2016-06-14 17:17:32 +0300

    T2  add trend detail label to dashboard tiles

--- src/components/Trend.js
+++ src/components/Trend.js
@@ -3,18 +3,32 @@ import React, { Component } from "react"
 import DashboardTile from "./DashboardTile"
 
 export default class Trend extends Component {
+    getTrendStyleByDelta(delta) {
+        if(this.props.trend.delta == null) return {className: "fa-ellipsis-h icon-success", label: "new"}
+
+        if(this.props.trend.delta == 0) return {className: "fa-minus icon-warning", label: "no change"}
+
+        if(this.props.trend.delta > 0) return {className: "fa-long-arrow-up icon-success", label: `+${delta}`}
+
+        if(this.props.trend.delta < 0) return {className: "fa-long-arrow-down icon-danger", label: `${delta}`}
+    }
+
     render() {
-
-        let arrowClassName = "fa-minus icon-warning"
-
-        if(this.props.trend.delta == null) arrowClassName = "fa-ellipsis-h icon-success"
-        if(this.props.trend.delta && this.props.trend.delta > 0) arrowClassName = "fa-long-arrow-up icon-success"
-        if(this.props.trend.delta && this.props.trend.delta < 0) arrowClassName = "fa-long-arrow-down icon-danger"
+        let trendStyle = this.getTrendStyleByDelta(this.props.trend.delta)
 
         return (
             <DashboardTile title={this.props.trend.alert.title || "No name!"} href={`/alerts/id/${this.props.trend.alert.id}`}>
-                <div className="text-center">
-                    <i className={`fa fa-5x trend-icon ${arrowClassName}`}></i>
+                <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-5 text-center">
+                        <i className={`fa fa-5x fa-fw trend-icon ${trendStyle.className}`}></i>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="trend-detail">
+                            {trendStyle.label}
+                        </div>
+
+                    </div>
                 </div>
             </DashboardTile>
         )
--- src/components/Trends.js
+++ src/components/Trends.js
@@ -14,10 +14,10 @@ export default class Trends extends Component {
                 </DashboardTile>
 
                 <Trend trend={{delta: 0, alert: {title: "Alert 1", id: 1}}}/>
-                <Trend trend={{delta: -4, alert: {title: "Alert 2", id: 2}}}/>
-                <Trend trend={{delta: 2, alert: {title: "Alert 3", id: 3}}}/>
-                <Trend trend={{delta: 4, alert: {title: "Alert 4", id: 4}}}/>
-                <Trend trend={{delta: -5, alert: {title: "Alert 5", id: 5}}}/>
+                <Trend trend={{delta: -4000, alert: {title: "Alert 2", id: 2}}}/>
+                <Trend trend={{delta: 200, alert: {title: "Alert 3", id: 3}}}/>
+                <Trend trend={{delta: 40000, alert: {title: "Alert 4", id: 4}}}/>
+                <Trend trend={{delta: -5000, alert: {title: "Alert 5", id: 5}}}/>
                 <Trend trend={{alert: {title: "Alert 6", id: 6}}}/>
 
                 {this.props.trends &&
