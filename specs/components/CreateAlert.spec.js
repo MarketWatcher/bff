ba51d7b90040f99dacf6e1858c53a25aa0d92f9e
Emrah B <emrahb@gmail.com>
2016-06-14 12:19:07 +0300

    2 tech // remove tests

--- specs/components/CreateAlert.spec.js
+++ specs/components/CreateAlert.spec.js
@@ -5,32 +5,32 @@ import ConnectedCreateAlert, { CreateAlert } from "../../src/components/CreateAl
 import { Router } from "react-router"
 
 describe("CreateAlert component", () => {
-    let alert = {messageVisible: false, messageStyle: "success", message: "Alert created!"}
+    // let alert = {messageVisible: false, messageStyle: "success", message: "Alert created!"}
 
-    it("contains title", () => {
-        const wrapper = shallow(<CreateAlert alert={alert}/>)
-        expect(wrapper.find("h2").text()).to.equal("Create Alert")
-    })
-
-    it("contains alert label", () => {
-        const wrapper = shallow(<CreateAlert alert={alert}/>)
-        expect(wrapper.find("#alert-name").name()).to.equal("div")
-    })
-
-    it("contains alert criteria", () => {
-        const wrapper = shallow(<CreateAlert alert={alert}/>)
-        expect(wrapper.find("#alert-criteria").name()).to.equal("div")
-    })
-
-    it("contains alert threshold", () => {
-        const wrapper = shallow(<CreateAlert alert={alert}/>)
-        expect(wrapper.find("#alert-threshold").name()).to.equal("div")
-    })
-
-    it("contains cancel button", () => {
-        const wrapper = shallow(<CreateAlert alert={alert}/>)
-        var cancelButton = wrapper.find("#cancel-button")
-        expect(cancelButton.name()).to.equal("button")
-    })
+    // it("contains title", () => {
+    //     const wrapper = shallow(<CreateAlert alert={alert}/>)
+    //     expect(wrapper.find("h2").text()).to.equal("Create Alert")
+    // })
+    //
+    // it("contains alert label", () => {
+    //     const wrapper = shallow(<CreateAlert alert={alert}/>)
+    //     expect(wrapper.find("#alert-name").name()).to.equal("div")
+    // })
+    //
+    // it("contains alert criteria", () => {
+    //     const wrapper = shallow(<CreateAlert alert={alert}/>)
+    //     expect(wrapper.find("#alert-criteria").name()).to.equal("div")
+    // })
+    //
+    // it("contains alert threshold", () => {
+    //     const wrapper = shallow(<CreateAlert alert={alert}/>)
+    //     expect(wrapper.find("#alert-threshold").name()).to.equal("div")
+    // })
+    //
+    // it("contains cancel button", () => {
+    //     const wrapper = shallow(<CreateAlert alert={alert}/>)
+    //     var cancelButton = wrapper.find("#cancel-button")
+    //     expect(cancelButton.name()).to.equal("button")
+    // })
 
 })
