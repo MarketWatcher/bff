58ba95282d4c65d96e94b3ea807f86391ccfb42d
Cengiz Can <cengizc@gmail.com>
2016-06-23 14:52:34 +0300

    #0 add safety guard to dockerize script

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -5,6 +5,9 @@ OLDPWD=`pwd`
 
 cd $DIR/../
 
+npm run clean
+npm run build
+
 docker login -u $DOCKER_USERNAME -e $DOCKER_EMAIL -p $DOCKER_PASSWORD
 docker build -t thoughtworksturkey/marketwatcher-io .
 docker push thoughtworksturkey/marketwatcher-io
