8dc60b4e4f0568bd70d7b1fd3b8d001a48009b9f
Zehra Kavasoglu <zehra@euzehra.local>
2016-06-23 12:20:17 +0300

    T3 @cengizIO add exit code check and service_name conf

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,10 +13,28 @@ ecs-cli configure \
 
 # ecs-cli up --keypair marketwatcher --capability-iam --size 1 --instance-type t2.medium
 
-ecs-cli compose --file docker-compose.yml service down
+CONFIGURE_RESULT=$?
+if [ $CONFIGURE_RESULT -ne 0 ]; then
+	echo "Could not configure ECS CLI"
+	exit $CONFIGURE_RESULT
+fi
 
-sleep 10
-
+COMPOSE_PROJECT_NAME=marketwatcher-io \
 ecs-cli compose --file docker-compose.yml service up
 
+UP_RESULT=$?
+if [ $UP_RESULT -ne 0 ]; then
+	echo "Could not bring service UP in ECS"
+	exit $UP_RESULT
+fi
+
 cd $OLDPWD
+
+
+
+
+
+
+
+
+
