e7a1879da942bc84f11ade3638adf6849bf16c46
kavasoglu <kavasoglu@gmail.com>
2016-06-27 12:44:53 +0000

    #8 @sinan-si change trend api content

--- src/api/trend-api.js
+++ src/api/trend-api.js
@@ -1,9 +1,21 @@
 export default class TrendApi {
     static list(userId, cb, err) {
-        let alertServiceURL = `/api/trends/user/${userId}`
-        fetch(alertServiceURL)
-        .then(res => res.json())
-        .then(json => cb(json))
+        let trendServiceURL = `/api/alerts/owner_id/${userId}`
+        fetch(trendServiceURL)
+        .then(res => { return res.json() })
+        .then(json => {
+        	let alerts = json.map((alert) => {
+        		return {
+        			delta: 0,
+        			alert: alert
+        		}
+        	})
+
+        	return cb(alerts)
+        })
         .catch(ex => err(ex))
     }
+
+
 }
+	
